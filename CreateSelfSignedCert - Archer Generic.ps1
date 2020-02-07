6c19166c3198c3528682aacd795cd6076c3e4ced
Aaron T <athumme@wgu.edu>
2020-02-07 12:25:21 -0600

    Add files via upload

---
+++ CreateSelfSignedCert - Archer Generic.ps1
@@ -0,0 +1,9 @@
+﻿$cert = New-SelfSignedCertificate -FriendlyName "Archer Generic Cert" -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
+
+$pwd = ConvertTo-SecureString -String "Password123$" -Force -AsPlainText
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+
+Export-PfxCertificate -Cert $cert -FilePath $DesktopPath\cert.pfx -Password $pwd
+
+Import-PfxCertificate -FilePath $DesktopPath\cert.pfx -CertStoreLocation Cert:\LocalMachine\Root -Password $pwd
--- CreateSelfSignedCert - Archer Web.ps1
+++ CreateSelfSignedCert - Archer Web.ps1
@@ -1,7 +1,9 @@
-$cert = New-SelfSignedCertificate -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -FriendlyName "Archer Web" -KeyExportPolicy Exportable -NotAfter (Get-Date).AddYears(5)
+$cert = New-SelfSignedCertificate -FriendlyName "Archer Web" -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
 
-#If you want to export the cert with a password, uncomment and run the following
-#
-#$pwd = ConvertTo-SecureString -String "MyPassword" -Force -AsPlainText
-#
-#Export-PfxCertificate -Cert $cert -FilePath C:\temp\cert.pfx -Password $pwd
+$pwd = ConvertTo-SecureString -String "Password123$" -Force -AsPlainText
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+
+Export-PfxCertificate -Cert $cert -FilePath $DesktopPath\cert.pfx -Password $pwd
+
+Import-PfxCertificate -FilePath $DesktopPath\cert.pfx -CertStoreLocation Cert:\LocalMachine\Root -Password $pwd
