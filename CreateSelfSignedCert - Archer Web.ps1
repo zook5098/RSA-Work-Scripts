7d372deebef76c7872267b948f83a99814e58a74
Aaron T <athumme@wgu.edu>
2020-02-07 10:07:05 -0600

    Add files via upload

--- CreateSelfSignedCert - Archer Web.ps1
+++ CreateSelfSignedCert - Archer Web.ps1
@@ -1,1 +1,7 @@
-New-SelfSignedCertificate -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -FriendlyName "Archer Web" -KeyExportPolicy Exportable -NotAfter (Get-Date).AddYears(5)
+$cert = New-SelfSignedCertificate -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -FriendlyName "Archer Web" -KeyExportPolicy Exportable -NotAfter (Get-Date).AddYears(5)
+
+#If you want to export the cert with a password, uncomment and run the following
+#
+#$pwd = ConvertTo-SecureString -String "MyPassword" -Force -AsPlainText
+#
+#Export-PfxCertificate -Cert $cert -FilePath C:\temp\cert.pfx -Password $pwd
