6a3489d805b818e07429c1d4752468a8ad47094d
Aaron T <athumme@wgu.edu>
2020-02-07 12:47:20 -0600

    Add files via upload

--- CreateSelfSignedCert - Archer Generic.ps1
+++ CreateSelfSignedCert - Archer Generic.ps1
@@ -1,4 +1,8 @@
-﻿$cert = New-SelfSignedCertificate -FriendlyName "Archer Generic Cert" -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
+﻿# Make sure you run this script from a Powershel Admin Prompt!
+# Make sure Powershell Execution Policy is bypassed to run these scripts!
+#Set-ExecutionPolicy Bypass -Scope Proces
+
+$cert = New-SelfSignedCertificate -FriendlyName "Archer Generic Cert" -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
 
 $pwd = ConvertTo-SecureString -String "Password123$" -Force -AsPlainText
 
