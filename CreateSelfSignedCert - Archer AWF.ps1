f61f0ce4e757a72417f079a5a3b7f55c18238043
Aaron T <athumme@wgu.edu>
2020-02-07 12:56:55 -0600

    Add files via upload

--- CreateSelfSignedCert - Archer AWF.ps1
+++ CreateSelfSignedCert - Archer AWF.ps1
@@ -1,6 +1,6 @@
 # Make sure you run this script from a Powershel Admin Prompt!
 # Make sure Powershell Execution Policy is bypassed to run these scripts!
-#Set-ExecutionPolicy Bypass -Scope Proces
+# Set-ExecutionPolicy -ExecutionPolicy Bypass
 
 $cert = New-SelfSignedCertificate -FriendlyName "Archer AWF" -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
 
