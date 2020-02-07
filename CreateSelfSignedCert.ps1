7f6138762cff3ca892fb50a4433a6305c63ca176
Aaron T <athumme@wgu.edu>
2020-02-07 12:40:01 -0600

    Delete CreateSelfSignedCert.ps1

--- CreateSelfSignedCert.ps1
+++
@@ -1,7 +0,0 @@
-$cert = New-SelfSignedCertificate -Subject “all.archer.local” -DnsName “all.archer.local”, “all” -CertStoreLocation “cert:\LocalMachine\My” -KeyExportPolicy Exportable -NotAfter (Get-Date).AddYears(5)
-
-#If you want to export the cert with a password, uncomment and run the following
-#
-#$pwd = ConvertTo-SecureString -String "MyPassword" -Force -AsPlainText
-#
-#Export-PfxCertificate -Cert $cert -FilePath C:\temp\cert.pfx -Password $pwd
