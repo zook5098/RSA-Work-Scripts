cc71e76d289678af19725614c31f490661ec2743
Aaron T <athumme@wgu.edu>
2020-03-02 12:41:55 -0600

    Add files via upload

---
+++ Cert Bound to AWF 8443.ps1
@@ -0,0 +1,5 @@
+﻿$certHash = netsh http show sslcert 0.0.0.0:8443 | Select-String "Certificate Hash"
+
+$thumbprint = $certHash -replace ".*: "
+
+Get-ChildItem -Path Cert:\LocalMachine\* -Recurse -Force | where {$_.Thumbprint -eq "$thumbprint"}
