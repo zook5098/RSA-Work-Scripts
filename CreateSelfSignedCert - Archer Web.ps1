1d7b3b334ee6b4b7f00510282baa605ee0c0846d
Aaron T <athumme@wgu.edu>
2020-03-02 11:59:57 -0600

    Update CreateSelfSignedCert - Archer Web.ps1

--- CreateSelfSignedCert - Archer Web.ps1
+++ CreateSelfSignedCert - Archer Web.ps1
@@ -1,13 +1,13 @@
 # Make sure you run this script from a Powershel Admin Prompt!
 # Make sure Powershell Execution Policy is bypassed to run these scripts!
-# Set-ExecutionPolicy -ExecutionPolicy Bypass
+Set-ExecutionPolicy -ExecutionPolicy Bypass
 
-$cert = New-SelfSignedCertificate -FriendlyName "Archer Web" -Subject ìall.archer.localî -DnsName ìall.archer.localî, ìallî -CertStoreLocation ìcert:\LocalMachine\Myî -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
+$cert = New-SelfSignedCertificate -FriendlyName "Archer Web" -Subject ‚Äúall.archer.local‚Äù -DnsName ‚Äúall.archer.local‚Äù -CertStoreLocation ‚Äúcert:\LocalMachine\My‚Äù -HashAlgorithm sha512 -KeyAlgorithm RSA -KeyLength 4096 -KeyExportPolicy Exportable -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.1") -NotAfter (Get-Date).AddYears(5)
 
 $pwd = ConvertTo-SecureString -String "Password123$" -Force -AsPlainText
 
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
 
-Export-PfxCertificate -Cert $cert -FilePath $DesktopPath\cert.pfx -Password $pwd
+Export-PfxCertificate -Cert $cert -FilePath $DesktopPath\Web_Cert.pfx -Password $pwd
 
-Import-PfxCertificate -FilePath $DesktopPath\cert.pfx -CertStoreLocation Cert:\LocalMachine\Root -Password $pwd
+Import-PfxCertificate -FilePath $DesktopPath\Web_Cert.pfx -CertStoreLocation Cert:\LocalMachine\Root -Password $pwd
