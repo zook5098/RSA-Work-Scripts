741d71c6fabc5502e48e424191114b58a5e17425
Aaron T <athumme@wgu.edu>
2020-03-02 12:39:27 -0600

    Delete Upgrade - Start Services.bat

--- Upgrade - Start Services.bat
+++
@@ -1,9 +0,0 @@
-echo
-
-net start RSAArcherConfigurationService
-net start RSAArcherJobEngine
-net start RSAArcherLDAPSynchronizationService
-net start RSAArcherQueuingService
-net start RSAArcherWorkflowService
-
-iisreset /start
